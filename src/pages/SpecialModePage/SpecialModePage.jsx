4f4504d67e5903596f31b4b4782a13b38087a843
Bogdan_141 <bogdanmakovey1001@gmail.com>
2026-05-23 22:35:25 +0200

    b

--- src/pages/SpecialModePage/SpecialModePage.jsx
+++ src/pages/SpecialModePage/SpecialModePage.jsx
@@ -6306,7 +6306,7 @@ export default function SpecialModePage() {
                                 const leftPct = Math.min(100, Math.max(0, leftWinProb * 100));
                                 const rightPct = 100 - leftPct;
 
-                                const blend = 5;
+                                const blend = 20;
 
                                 const start = Math.max(0, leftPct - blend);
                                 const end = Math.min(100, leftPct + blend);
