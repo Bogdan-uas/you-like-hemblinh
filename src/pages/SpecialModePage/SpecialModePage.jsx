cafd36495d417c982ec2b6dc3547eaa6f6cc41fb
Bogdan_141 <bogdanmakovey1001@gmail.com>
2026-05-25 12:51:04 +0200

    b

--- src/pages/SpecialModePage/SpecialModePage.jsx
+++ src/pages/SpecialModePage/SpecialModePage.jsx
@@ -6401,7 +6401,7 @@ export default function SpecialModePage() {
 
                                 const placementDiff = rightPlacement - leftPlacement;
 
-                                const placementWeight = 0.035;
+                                const placementWeight = 0.5;
 
                                 const placementBonus = placementDiff * placementWeight;
 
