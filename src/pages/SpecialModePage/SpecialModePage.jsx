b550bda3f89e62559bc5e32007107ed8b38ac0cd
Bogdan_141 <bogdanmakovey1001@gmail.com>
2026-06-14 17:49:34 +0200

    this may get reverted

--- src/pages/SpecialModePage/SpecialModePage.jsx
+++ src/pages/SpecialModePage/SpecialModePage.jsx
@@ -4602,6 +4602,7 @@ function SpecialModePage() {
                                             className={css.round_text}
                                             style={{
                                                 fontSize: "12px",
+                                                shadow: `${leftScore > rightScore ? seriesState.leftTeam.shadow : seriesState.rightTeam.shadow}`
                                             }}
                                         >
                                             Set {set}
