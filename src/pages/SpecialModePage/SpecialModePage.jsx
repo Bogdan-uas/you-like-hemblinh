370e8c88a322a2ac8e1fdb1e99889c991473dd5a
Bogdan_141 <bogdanmakovey1001@gmail.com>
2026-07-29 16:52:58 +0200

    b

--- src/pages/SpecialModePage/SpecialModePage.jsx
+++ src/pages/SpecialModePage/SpecialModePage.jsx
@@ -6005,7 +6005,7 @@ function SpecialModePage() {
                                 columnGap: "12px",
                                 rowGap: '4px',
                                 maxWidth: "380px",
-                                height: setsToWin * 2 - 1 >= 5 ? "62px" : "40px",
+                                height: seriesState.finishedSets?.length >= 7 ? "62px" : "40px",
                             }}
                         >
                             {seriesState.finishedSets.map(({
